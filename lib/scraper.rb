b942f5fb77e756a394647da28f967c2e23e15d6b
Shami Tomita <shamitomita@gmail.com>
2021-02-03 16:03:37 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -42,7 +42,16 @@ class Scraper
   end 
   
   def print_courses
-  end 
+    self.make_courses
+    Course.all.each do |course|
+      if course.title && course.title != ""
+        puts "Title: #{course.title}"
+        puts "  Schedule: #{course.schedule}"
+        puts "  Description: #{course.description}"
+      end
+    end
+  end
+ 
   
 end
 
